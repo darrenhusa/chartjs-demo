843e4523da3d1dd4948e158730275ea230d6e6ff
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-06-06 09:43:01 -0500

    Trying to troubleshoot. Still a WIP.

---
+++ resources/js/components/Graph.vue
@@ -0,0 +1,52 @@
+import Chart from 'chart.js';
+
+
+export default {
+    template: '<canvas width="600" height="400"></canvas>',
+
+    <!-- data: {
+      labels: ['January', 'Febuary', 'March'],
+
+      datasets: [
+        {
+          label: 'revenue',
+          // backgroundColor: 'blue',
+          backgroundColor: 'rgb(200, 220, 220, 0.2)',
+          // borderColor: 'red',
+          borderColor: 'rgb(220, 220, 220, 0.2)',
+          // borderWidth: 1,
+
+          data: [30, 122, 90]
+        }
+      ]
+    }, -->
+
+    <!-- options: {}, -->
+
+    ready() {
+      var context = document.getElementById('graph').getContext('2d');
+
+      var myChart = new Chart(context, {
+        type: 'line',
+        data: {
+          labels: ['January', 'Febuary', 'March'],
+
+          datasets: [
+            {
+              label: 'revenue',
+              // backgroundColor: 'blue',
+              backgroundColor: 'rgb(200, 220, 220, 0.2)',
+              // borderColor: 'red',
+              borderColor: 'rgb(220, 220, 220, 0.2)',
+              // borderWidth: 1,
+
+              data: [30, 122, 90]
+            }
+          ]
+        },
+        
+        options: {},
+      });
+
+    }
+}
